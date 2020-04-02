4a5d858e395467b44aee9b7a6d240d788b955215
Robert Henryk Zawiasa <robert.zawiasa@gmail.com>
2020-04-02 17:36:16 +0200

    402a

---
+++ project.clj
@@ -0,0 +1,13 @@
+(defproject beagle "x.y.z"
+    :dependencies [[org.clojure/clojure "1.9.0"]
+                   [org.clojure/core.rrb-vector "0.0.11"]
+;                  [org.clojure/data.priority-map "0.0.10"]
+                   [org.flatland/ordered "1.5.6"]
+                  ]
+    :plugins [[lein-try "0.4.3"]]
+;   :global-vars {*warn-on-reflection* true}
+    :jvm-opts ["-Xmx6g"]
+    :javac-options ["-g"]
+    :source-paths ["src"] :java-source-paths ["src"] :resource-paths ["resources"] :test-paths ["src"]
+;   :main beagle.core
+    :aliases {"beagle" ["run" "-m" "beagle.core"]})
